5842fdc580cbe4d45cb627babf8bb6462e44ea01
DyoRizqal <dyorizqal7@gmail.com>
2024-11-23 21:20:23 +0700

    Setting build.gradle.kts, add model dataPegawai

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,9 @@
 plugins {
-    alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android)
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("kotlin-parcelize")
+    id("kotlin-kapt")
+    id("com.google.android.libraries.mapsplatform.secrets-gradle-plugin") version "2.0.1" apply false
 }
 
 android {
@@ -19,6 +22,7 @@ android {
 
     buildFeatures {
         viewBinding = true
+        buildConfig = true
     }
 
     buildTypes {
@@ -31,24 +35,94 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
 }
 
 dependencies {
 
+    // AndroidX Core
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
-    implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.swiperefreshlayout)
+    implementation(libs.androidx.activity.ktx)
+
+    // Paging & Room
+    implementation(libs.androidx.paging.runtime.ktx)
+    implementation(libs.androidx.room.paging)
+    implementation(libs.androidx.room.runtime)
+    implementation(libs.espresso.idling.resource)
+    kapt(libs.androidx.room.compiler)
+    implementation(libs.androidx.room.ktx)
+
+    // Lifecycle
+    implementation(libs.androidx.lifecycle.livedata.ktx)
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
+
+    // DataStore
+    implementation(libs.androidx.datastore.preferences)
+
+    // Glide
+    implementation(libs.glide)
+    kapt(libs.compiler)
+
+    // Image Compression
+    implementation(libs.compressor)
+
+    // API (Retrofit & OkHttp)
+    implementation(libs.retrofit)
+    implementation(libs.converter.gson)
+    implementation(libs.logging.interceptor)
+    implementation(libs.converter.moshi)
+    implementation(libs.moshi.kotlin)
+
+    // Coroutine
+    implementation(libs.kotlinx.coroutines.android)
+
+    // CircleImageView (Optional)
+    implementation(libs.circleimageview)
+
+    // CameraX (Optional)
+    implementation(libs.androidx.camera.core)
+    implementation(libs.androidx.camera.camera2)
+    implementation(libs.androidx.camera.lifecycle)
+    implementation(libs.androidx.camera.view)
+
+    // Maps
+    implementation(libs.play.services.maps)
+    implementation(libs.play.services.location)
+
+    // Testing - Unit Tests
     testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
+    testImplementation(libs.mockito.core)
+    testImplementation(libs.mockito.inline)
+    testImplementation(libs.mockwebserver)
+    testImplementation(libs.androidx.core.testing)
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.9.0")
+    testImplementation("io.mockk:mockk:1.13.8")
+    testImplementation("androidx.paging:paging-common:3.3.4")
+
+    // Testing - UI Tests
+    androidTestImplementation("androidx.test:core:1.5.0")
+    androidTestImplementation("androidx.test:runner:1.5.2")
+    androidTestImplementation("androidx.test:rules:1.5.0")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("androidx.test.espresso:espresso-contrib:3.5.1")
+    androidTestImplementation("androidx.test.espresso:espresso-idling-resource:3.5.1")
+    androidTestImplementation("androidx.test.espresso:espresso-intents:3.5.1")
+    androidTestImplementation("androidx.test.espresso:espresso-accessibility:3.5.1")
+    androidTestImplementation("org.mockito:mockito-android:4.11.0")
+    androidTestImplementation("androidx.test.espresso:espresso-idling-resource:3.5.1")
+
+    // Debugging Tools
+    debugImplementation(libs.androidx.fragment.testing)
 
 //  Navbar
     implementation(libs.smoothbottombar)
---
+++ app/src/main/java/com/capstoneproject/aji/model/dataPegawai.kt
@@ -0,0 +1,14 @@
+package com.capstoneproject.aji.model
+
+data class DataPegawai(
+    val id_pegawai: Int,
+    val user_id: Int,
+    val nik: String,
+    val ttl: String,
+    val alamat: String,
+    val jenis_kelamin: String,
+    val no_wa: String,
+    val no_rek: String,
+    val agama: String,
+    val posisi: String
+)
